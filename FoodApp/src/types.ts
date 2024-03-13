87152bc54211dc345d09082091e6243a20625af1
aldinalj <aldinalj@gmail.com>
2024-03-13 11:07:13 +0100

    Types.ts added

---
+++ FoodApp/src/types.ts
@@ -0,0 +1,26 @@
+export interface Recipe {
+    _id?: string 
+
+    title: string,
+    description: string,
+
+    rating?: Rating,
+    imageUrl: string,
+    timeInMins?: number,
+    categories: string[], 
+    instructions: string[],
+    completed?: boolean,
+    ingredients: Ingredient
+}
+
+
+export interface Ingredient {
+    name: string,
+    amount: number,
+    unit: string
+}
+
+
+export interface Rating {
+    rating: number,
+}
