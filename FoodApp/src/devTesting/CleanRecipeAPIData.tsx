051852901f9753bf94fe6d9a4fd33f8e1832a8ff
FredrikEisermanWendt <fredrikeiserman@gmail.com>
2024-03-21 15:59:00 +0100

    component ready fo testing, added devPages folder in devTesting, added testPageCleanRecipes.tsx to devPages, added component to page

--- FoodApp/src/devTesting/CleanRecipeAPIData.tsx
+++ FoodApp/src/devTesting/CleanRecipeAPIData.tsx
@@ -1,13 +1,56 @@
+import useRecipeState from "../states/StateManager";
+import recipesData from "./stockRecipes.json";
+import "../resources/Variables";
+import { getURL } from "../resources/Variables";
+import axios from "axios";
 
 const CleanRecipeAPIData = () => {
+  const recipeState = useRecipeState();
+  const recipeURL = getURL();
 
+  //funktion to uppload full json file
+  const uploadRecipeJson = async () => {
+    for (const recipe of recipesData) {
+      try {
+        const response = await axios.post(`${recipeURL}/recipes`, recipe);
+        if (response.status === 200) {
+          console.log(response.data);
+        }
+      } catch (error) {
+        console.error("Error, upload failed");
+      }
+    }
+  };
+
+  //function to uppload json and uppdate state
+  const addDefaultRecipeData = () => {
+    uploadRecipeJson();
+    recipeState.getRecipesFromAPI();
+  };
+
+  const clearAPI = async () => {
+    try {
+      const response = await axios.get(`${recipeURL}/clear`);
+      console.log(response.status);
+    } catch (error) {
+      console.error("Error, clear failed");
+    }
+  };
+
+  const handleUploadClick = () => {
+    addDefaultRecipeData();
+  };
+
+  const handleClearAPIClick = () => {
+    clearAPI();
+  };
 
   return (
     <div>
-        <button>Add Default Recipe Data</button>
-        <button>Clear API From All Data</button>
+      <button onClick={handleUploadClick}>Add Default Recipe Data</button>
+      <button onClick={handleClearAPIClick}>Clear API From All Data</button>
     </div>
-  )
-}
+  );
+};
 
-export default CleanRecipeAPIData
+export default CleanRecipeAPIData;
---
+++ FoodApp/src/devTesting/devPages/testPageCleanRecipes.tsx
@@ -0,0 +1,11 @@
+import CleanRecipeAPIData from "../CleanRecipeAPIData"
+
+const testPageCleanRecipes = () => {
+  return (
+    <div>
+      <CleanRecipeAPIData/>
+    </div>
+  )
+}
+
+export default testPageCleanRecipes
