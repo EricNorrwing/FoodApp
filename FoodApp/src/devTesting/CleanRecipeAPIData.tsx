f26d92dab582e68968cecd5fcc0a921922e790b5
FredrikEisermanWendt <fredrikeiserman@gmail.com>
2024-03-21 14:56:43 +0100

    added devTesting folder, added CleanRecipeAPIData.tsx, added stockRecipe.json, filled json with recipes

---
+++ FoodApp/src/devTesting/CleanRecipeAPIData.tsx
@@ -0,0 +1,13 @@
+
+const CleanRecipeAPIData = () => {
+
+
+  return (
+    <div>
+        <button>Add Default Recipe Data</button>
+        <button>Clear API From All Data</button>
+    </div>
+  )
+}
+
+export default CleanRecipeAPIData
