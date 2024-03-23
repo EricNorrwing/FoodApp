63264765d411bc8b4a659d4a4871e1fb3f86ae5e
AlexP2508 <alexander@pumarin.se>
2024-03-23 08:41:27 +0100

    added Routing in App.tsx

--- FoodApp/src/App.tsx
+++ FoodApp/src/App.tsx
@@ -1,21 +1,23 @@
-import "./App.css";
-import { Recipe, Ingredient, Ratings } from "./types";
-import useRecipeState from "./states/StateManager";
-import CreateRecipeComponent from "./components/CreateRecipeComponent";
-import { useState } from "react";
-import HomePage from "./pages/HomePage";
 import CreateRecipePage from "./pages/CreateRecipePage";
-import { BrowserRouter } from "react-router-dom";
-import { Routes, Route } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import HomePage from "./pages/HomePage";
+/* import HomePage from "./pages/HomePage2"; */
 function App() {
   return (
     <>
       <BrowserRouter>
         {/* <Header/> */}
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          {/* <Route path="/" element={<HomePage />} /> */}
+          {/* <Route path="/" element={<HomePage2 />} /> 
+          Searchbar logic which needs to be moved to Searchbar Component so I can integrate it to Header/Navbar*/}
           <Route path="/create-recipe" element={<CreateRecipePage />} />
+          {/* <Route path="/recipes" element={<AllRecipes />} />
+          <Route path="/cocktails" element={<AllCocktails />} />
+          <Route path="/recipes/:_id" element={<SingleRecipe />} />
+          <Route path="/categories/:_id" element={<AllCategories />} />
+          <Route path="about-us" element={<About />} />
+          <Route path="contact-us" element={<ContactPage />} /> */}
         </Routes>
         {/* <Footer/> */}
       </BrowserRouter>
